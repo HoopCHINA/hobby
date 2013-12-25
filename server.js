794a0a03b4bd1198905936d72c133d2ddcd73451
Sopl'Wang <sopl.wang@gmail.com>
2013-12-25 21:59:00 +0800

    Update restify and remove admin support

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 var restify = require('restify');
 var http = require('http');
 var https = require('https');
-var path = require('path');
 
 // Manual set `maxSockets` to 10 for limit concurrents
 http.globalAgent.maxSockets = 10;
@@ -14,24 +13,18 @@ var port = process.env['HOBBY_PORT'] || 3000;
 
 var server = restify.createServer({
   name: 'hobby',
-  version: '0.0.2',
+  version: '0.0.3',
 });
 
 server.pre(restify.pre.sanitizePath());
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.bodyParser({ mapParams: false }));
-server.use(restify.jsonp());
 server.use(restify.requestLogger());
 
 // Service
 require('./lib/tasks').bootstrap();
 // Routes
 require('./routes')(server);
-// Assets (PATCH: make `serveStatic()` look in correct dir for 'default' file)
-server.get(/\/(admin|js|css|images|fonts)($|\/.*)/, restify.serveStatic({
-  'directory': path.join(__dirname, 'public'),
-  'default': 'index.html',
-}));
 
 server.listen(port, addr, function () {
   console.log('%s listening at %s', server.name, server.url);
